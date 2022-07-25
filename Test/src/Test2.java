61a44fe8d55d264caeabd33c3bcdad1632dc7c01
sttoraus <65400254+sttoraus@users.noreply.github.com>
2022-07-25 22:28:21 +0200

    Update Test2.java

--- Test/src/Test2.java
+++ Test/src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0;i<10;i++) {
-			System.out.println("Hallo Welt");
+			System.out.println(i);
 		}
 
 	}
